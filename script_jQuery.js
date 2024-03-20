925b1605593ee47dc0d57eee56f64c00275baac8
Victor Okechukwu <vicoke360@gmail.com>
2024-03-20 17:36:02 +0100

    Updates made

--- script_jQuery.js
+++ script_jQuery.js
@@ -11,9 +11,11 @@ const tipInput = $("#tip");
 const totalInput = $("#total");
 const custom = $("#custom");
 const custom_1 = $("#custom_1");
+const custom_1_input = $("#custom_1 > input");
+const custom_1_img = $("#custom_1 > img");
 const billContainer = $("#billValue");
 const peopleContainer = $("#people");
-const reset = $("button")
+const reset = $("button");
 let bill;
 let people;
 let tip;
@@ -21,9 +23,9 @@ let mobileView;
 let desktopView;
 console.log(window.innerWidth);
 if (window.innerWidth <= 680) {
-  mobileView = window.innerWidth
+  mobileView = window.innerWidth;
 } else {
-  desktopView = window.innerWidth
+  desktopView = window.innerWidth;
 }
 // console.log(mobileView);
 // console.log(desktopView);
@@ -35,7 +37,6 @@ const percent_only = Array.from(all_percent).slice(0, -2);
 console.log(percent_only);
 console.log(all_percent);
 
-let currentElement = null;
 
 percent_only.forEach((element) => {
   let elementID = parseInt(element.id.slice(-2));
@@ -53,55 +54,51 @@ percent_only.forEach((element) => {
 
     // Remove 'clicked' class from all elements
     percent_only.forEach((el) => {
-      $(el).removeClass('clicked');
+      $(el).removeClass("clicked");
     });
 
-      custom.click(function() {
-          percent_only.forEach((el) => {
-          $(el).removeClass('clicked');
-        });
+    custom.click(function () {
+      percent_only.forEach((el) => {
+        $(el).removeClass("clicked");
       });
-    
-      
-      
-      $(document).keydown(function (e) { 
-        getCustomPercentageValue(e.key);
-      });
+    });
+
+    $(document).keydown(function (e) {
+      getCustomPercentageValue(e.key);
+    });
 
     // Add 'clicked' class to the clicked element
-    $(element).addClass('clicked');
+    $(element).addClass("clicked");
 
     const pCustom = $("#custom > p");
     console.log(pCustom);
     console.log("object");
     pCustom.css("display", "block");
-    custom_1.css( "display", "none");
+    custom_1.css("display", "none");
     
-
     if (
       Number($(billInput).val()) === 0 ||
       Number($(peopleInput).val()) === 0
-    ) {
-      $(billContainer).addClass("warning");
-      $(peopleContainer).addClass("warning");
-      console.log("rre");
+      ) {
+        $(billContainer).addClass("warning");
+        $(peopleContainer).addClass("warning");
+        console.log("Cannot be empty");
       return;
     } else {
       $(billContainer).removeClass("warning");
       $(peopleContainer).removeClass("warning");
     }
-    
+
     // customPercentage();
     tipCalculation(elementID);
     totalCalculation(elementID);
-    // console.log(currentElement)
   });
 });
 
 function tipCalculation(percent) {
   bill = parseFloat(billInput.val());
   people = parseFloat(peopleInput.val());
-  tip = ((bill) * percent / 100) / people;
+  tip = (bill * percent) / 100 / people;
   console.log(tip);
   tipInput.val(tip.toFixed());
 }
@@ -111,41 +108,42 @@ function totalCalculation(percent) {
   console.log(bill);
   people = parseFloat(peopleInput.val());
   console.log(people);
-  tip = (bill * (percent / 100) / people);
-  let total = (bill / people) + tip;
+  tip = (bill * (percent / 100)) / people;
+  let total = bill / people + tip;
   totalInput.val(total.toFixed(2));
   console.log(total);
 }
 
 function customPercentage() {
-  custom.click(function() {
+  custom.click(function () {
     const pCustom = $("#custom > p");
     console.log(pCustom);
     console.log("object");
     pCustom.css("display", "none");
-    custom_1.css( "display", "block" );
+    custom_1.css("display", "block");
   });
 
   percent_only.forEach((el) => {
-    $(el).removeClass('clicked');
+    $(el).removeClass("clicked");
   });
-  
-  $(document).keydown(function (e) { 
+
+  $(document).keydown(function (e) {
     getCustomPercentageValue(e.key);
   });
 
   if (mobileView) {
-    $("#custom_1").on("change", mobileLogic)
+    $("#custom_1_input").on("change", mobileLogic());
     // custom_1.change(mobileLogic())
   }
 }
 
-function getCustomPercentageValue(key){
-  if(key === 'Enter'){
+function getCustomPercentageValue(key) {
+  if (key === "Enter") {
     // console.log("kEY PRESSED");
     if (
       Number($(billInput).val()) === 0 ||
-      Number($(peopleInput).val()) === 0
+      Number($(peopleInput).val()) === 0 &&
+      Number($(custom_1_input).val()) === 0
     ) {
       $(billContainer).addClass("warning");
       $(peopleContainer).addClass("warning");
@@ -158,46 +156,82 @@ function getCustomPercentageValue(key){
     console.log(key);
     bill = parseFloat(billInput.val());
     people = parseFloat(peopleInput.val());
-    let percent = parseFloat(custom_1.val()/100);
+    let percent = parseFloat(custom_1_input.val() / 100);
     console.log(percent);
-    tip = ((bill) * percent) / people;
-    tipInput.val(tip.toFixed(2))
+    tip = (bill * percent) / people;
+    tipInput.val(tip.toFixed(2));
     let total = bill / people + tip;
     console.log(total);
     totalInput.val(total.toFixed(2));
   }
+
+  $(custom_1_img).click(function () {
+    if (
+      Number($(billInput).val()) === 0 || Number($(peopleInput).val()) === 0 || Number($(custom_1_input).val()) === 0) {
+      $(billContainer).addClass("warning");
+      $(peopleContainer).addClass("warning");
+      console.log("Cannot be empty");
+      // console.log("rre");
+      return;
+    } else {
+      $(billContainer).removeClass("warning");
+      $(peopleContainer).removeClass("warning");
+    }
+
+    bill = parseFloat(billInput.val());
+    people = parseFloat(peopleInput.val());
+    let percent = parseFloat(custom_1_input.val() / 100);
+    console.log(percent);
+    tip = (bill * percent) / people;
+    tipInput.val(tip.toFixed(2));
+    let total = bill / people + tip;
+    console.log(total);
+    totalInput.val(total.toFixed(2));
+  });
 }
 
 function mobileLogic() {
+  // if (
+  //   Number($(billInput).val()) === 0 ||
+  //   Number($(peopleInput).val()) === 0 &&
+  //   Number($(custom_1_input).val()) === 0
+  // ) {
+  //   $(billContainer).addClass("warning");
+  //   $(peopleContainer).addClass("warning");
+  //   console.log("rre");
+  //   return;
+  // } else {
+  //   $(billContainer).removeClass("warning");
+  //   $(peopleContainer).removeClass("warning");
+  // }
   console.log("mobile1");
   setTimeout(() => {
     console.log("mobile");
     bill = parseFloat(billInput.val());
     people = parseFloat(peopleInput.val());
-    let percent = parseFloat(custom_1.val()/100);
+    let percent = parseFloat(custom_1_input.val() / 100);
     console.log(percent);
-    tip = ((bill) * percent) / people;
-    tipInput.val(tip.toFixed(2))
+    tip = (bill * percent) / people;
+    tipInput.val(tip.toFixed(2));
     let total = bill / people + tip;
     console.log(total);
     totalInput.val(total.toFixed(2));
   }, 600);
-  // if(custom_1.val("")) {
+  // if(custom_1_input.val("")) {
   //   return;
   // }
 }
 
-// $(custom_1).change(mobileLogic)
+// $(custom_1_input).change(mobileLogic)
 
 function resetAll() {
   reset.click(() => {
     console.log("RESET Completed");
-    billInput.val('');
-    peopleInput.val('');
-    custom_1.val('');
-  })
+    billInput.val("");
+    peopleInput.val("");
+    custom_1_input.val("");
+  });
 }
 
-
 customPercentage();
-resetAll()
+resetAll();
